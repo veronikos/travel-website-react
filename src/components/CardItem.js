4e01c5e500a38b9a55c5407e09688923800b5a86
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-10-12 17:49:14 +0300

    add new section on homepage

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Cards.css";
 
 function CardItem() {
-    return (
-        <>
-            
-        </>
-    )
+  return (
+    <>
+      <li className="cards__item">
+        <Link className="cards__item__link" to='/'>
+          <figure className="cards__item__pic-wrap">
+            <img src="/" alt="Travel" className="cards__item__img" />
+          </figure>
+          <div className="cards__item__info">
+              <h5 className="cards__item__text"></h5>
+          </div>
+        </Link>
+      </li>
+    </>
+  );
 }
 
-export default CardItem
+export default CardItem;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,13 @@
 import '../../App.css'
 import HeroSection from '../HeroSection'
 import React from 'react'
+import Cards from '../Cards'
 
 export default function Home () {
     return (
         <>
         <HeroSection/>
+        <Cards />
         </>
     )
 }
