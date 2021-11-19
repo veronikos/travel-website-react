147e67791cc36819798ef802e747202412bdef6a
Veronika Kostenko PC <veronikostenko@gmail.com>
2021-11-19 12:01:53 +0200

    finished website. has issur of accessing image from App.css

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,7 +8,38 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-              <CardItem />
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the deep waterfall inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the islands of Bali"
+              label="Cruise"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-3.jpg"
+              text="The sea trip on a small private boat"
+              label="Deep Sea"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-4.jpg"
+              text="Come on and go for sports in the most spectacular view"
+              label="Island"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-5.jpg"
+              text="See the lonely streets of Oslo"
+              label="City Tour"
+              path="/services"
+            />
           </ul>
         </div>
       </div>
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,12 +2,14 @@ import '../../App.css'
 import HeroSection from '../HeroSection'
 import React from 'react'
 import Cards from '../Cards'
+import Footer from '../Footer'
 
 export default function Home () {
     return (
         <>
         <HeroSection/>
         <Cards />
+        <Footer/>
         </>
     )
 }
